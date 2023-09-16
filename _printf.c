0e64cb9efc317042a3d83583b54a13e757b7b903
maryannemm <maryannemaree@gmail.com>
2023-09-16 09:38:14 -0700

    reverse2

--- _printf.c
+++ _printf.c
@@ -10,56 +10,89 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0; /* To keep track of the number of characters printed */
+    va_list args;
+    int count = 0; /* To keep track of the number of characters printed */
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++; /* Move past '%' */
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; /* Move past '%' */
 
-			/* Handle conversion specifiers */
-			switch (*format)
-			{
-			case 'c':
-			{
-				char c = va_arg(args, int);
-				count += write(1, &c, 1);
-			}
-			break;
-			case 's':
-			{
-				const char *str = va_arg(args, const char *);
-				while (*str)
-				{
-					count += write(1, str, 1);
-					str++;
-				}
-			}
-			break;
-			case '%':
-				count += write(1, "%", 1);
-				break;
-			default:
-				/* Unsupported conversion specifier, just print it as is */
-				count += write(1, "%", 1);
-				count += write(1, format, 1);
-				break;
-			}
-		}
-		else
-		{
-			/* Regular character, just print it */
-			count += write(1, format, 1);
-		}
-		format++;
-	}
+            /* Handle conversion specifiers */
+            switch (*format)
+            {
+                case 'c':
+                {
+                    char c = va_arg(args, int);
+                    count += write(1, &c, 1);
+                }
+                break;
+                case 's':
+                {
+                    const char *str = va_arg(args, const char *);
+                    while (*str)
+                    {
+                        count += write(1, str, 1);
+                        str++;
+                    }
+                }
+                break;
+                case '%':
+                    count += write(1, "%", 1);
+                    break;
+                case 'd':
+                case 'i':
+                {
+                    int num = va_arg(args, int);
+                    char buffer[12]; /* Sufficient for INT_MIN */
+                    int len = 0;
 
-	va_end(args);
-	return (count);
+                    if (num < 0)
+                    {
+                        count += write(1, "-", 1);
+                        num = -num;
+                    }
+
+                    if (num == 0)
+                    {
+                        count += write(1, "0", 1);
+                    }
+                    else
+                    {
+                        while (num > 0)
+                        {
+                            buffer[len] = num % 10 + '0';
+                            num /= 10;
+                            len++;
+                        }
+
+                        while (len > 0)
+                        {
+                            len--;
+                            count += write(1, &buffer[len], 1);
+                        }
+                    }
+                }
+                break;
+                default:
+                    /* Unsupported conversion specifier, just print it as is */
+                    count += write(1, "%", 1);
+                    count += write(1, format, 1);
+                    break;
+            }
+        }
+        else
+        {
+            /* Regular character, just print it */
+            count += write(1, format, 1);
+        }
+        format++;
+    }
+
+    va_end(args);
+    return (count);
 }
-
 
