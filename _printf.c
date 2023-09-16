179009109c3cd9e909131e0146a5cabec8765fa5
maryannemm <maryannemaree@gmail.com>
2023-09-16 12:33:55 -0700

    corrections 5

--- _printf.c
+++ _printf.c
@@ -1,55 +1,96 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
-#include "print_functions.h"
-#include "write_functions.h"
+
 /**
  * _printf - Custom printf function.
  * @format: The format string.
  *
  * Return: The number of characters printed (excluding the null byte).
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-
+	int count = 0; /* To keep track of the number of characters printed */
+    
 	va_start(args, format);
 
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			format++;
+			format++; /* Move past '%' */
 			switch (*format)
 			{
 				case 'c':
-					count += print_char(va_arg(args, int));
-					break;
+				{
+					char c = va_arg(args, int);
+
+					count += write(1, &c, 1);
+				}
+				break;
 				case 's':
-					count += print_string(va_arg(args, const char *));
-					break;
+				{
+
+					const char *str = va_arg(args, const char *);
+					while (*str)
+					{
+						count += write(1, str, 1);
+						str++;
+					}
+				}
+				break;
 				case '%':
-					count += print_char('%');
+					count += write(1, "%", 1);
 					break;
 				case 'd':
 				case 'i':
-					count += print_int(va_arg(args, int));
-					break;
+				{
+					int num = va_arg(args, int);
+					char buffer[12]; /* Sufficient for INT_MIN */
+					int len = 0;
+
+					if (num < 0)
+					{
+						count += write(1, "-", 1);
+						num = -num;
+					}
+
+					if (num == 0)
+					{
+						count += write(1, "0", 1);
+					}
+					else
+					{
+						while (num > 0)
+						{
+							buffer[len] = num % 10 + '0';
+							num /= 10;
+							len++;
+						}
+
+						while (len > 0)
+						{
+							len--;
+							count += write(1, &buffer[len], 1);
+						}
+					}
+				}
+				break;
 				default:
-					count += print_char('%');
-					count += print_char(*format);
+					/* Unsupported conversion specifier, just print it as is */
+					count += write(1, "%", 1);
+					count += write(1, format, 1);
 					break;
 			}
 		}
 		else
 		{
-			count += print_char(*format);
+			/* Regular character, just print it */
+			count += write(1, format, 1);
 		}
 		format++;
 	}
-
 	va_end(args);
 	return (count);
 }
--- write_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef WRITE_FUNCTIONS_H
-#define WRITE_FUNCTIONS_H
-
-int write_char(char c);
-int write_str(const char *str);
-int write_int(int num);
-
-#endif /* WRITE_FUNCTIONS_H */
-
