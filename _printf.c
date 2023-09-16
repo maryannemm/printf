3df1c0eb605be9b6fa69a88eb628bd9105197301
maryannemm <maryannemaree@gmail.com>
2023-09-16 09:35:09 -0700

    reverse

--- _printf.c
+++ _printf.c
@@ -10,55 +10,56 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0; /* To keep track of the number of characters printed */
+	va_list args;
+	int count = 0; /* To keep track of the number of characters printed */
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++; /* Move past '%' */
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++; /* Move past '%' */
 
-            /* Handle conversion specifiers */
-            switch (*format)
-            {
-                case 'c':
-                {
-                    char c = va_arg(args, int);
-                    count += write(1, &c, 1);
-                }
-                break;
-                case 's':
-                {
-                    const char *str = va_arg(args, const char *);
-                    while (*str)
-                    {
-                        count += write(1, str, 1);
-                        str++;
-                    }
-                }
-                break;
-                case '%':
-                    count += write(1, "%", 1);
-                    break;
-                default:
-                    /* Unsupported conversion specifier, just print it as is */
-                    count += write(1, "%", 1);
-                    count += write(1, format, 1);
-                    break;
-            }
-        }
-        else
-        {
-            /* Regular character, just print it */
-            count += write(1, format, 1);
-        }
-        format++;
-    }
+			/* Handle conversion specifiers */
+			switch (*format)
+			{
+			case 'c':
+			{
+				char c = va_arg(args, int);
+				count += write(1, &c, 1);
+			}
+			break;
+			case 's':
+			{
+				const char *str = va_arg(args, const char *);
+				while (*str)
+				{
+					count += write(1, str, 1);
+					str++;
+				}
+			}
+			break;
+			case '%':
+				count += write(1, "%", 1);
+				break;
+			default:
+				/* Unsupported conversion specifier, just print it as is */
+				count += write(1, "%", 1);
+				count += write(1, format, 1);
+				break;
+			}
+		}
+		else
+		{
+			/* Regular character, just print it */
+			count += write(1, format, 1);
+		}
+		format++;
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
+
 
