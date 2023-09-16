ae740c338bcead9dd504a422638be8dcba5be2ea
maryannemm <maryannemaree@gmail.com>
2023-09-16 08:44:30 -0700

    m

--- _printf.c
+++ _printf.c
@@ -1,42 +1,65 @@
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * _printf - custom printf function
- * @format: format string
+ * _printf - Custom printf function.
+ * @format: The format string.
  *
- * Return: number of characters printed
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int num;
+	va_list args;
+	int count = 0; /* To keep track of the number of characters printed */
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
-        {
-            num = va_arg(args, int);
-            if (num < 0)
-            {
-                write(1, "-", 1);
-                num = -num;
-                count++;
-            }
-            count += write(1, num + '0', 1);
-        }
-        else
-        {
-            count += write(1, format, 1);
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++; /* Move past '%' */
 
-    va_end(args);
-    return (count);
+			/* Handle conversion specifiers */
+			switch (*format)
+			{
+			case 'c':
+			{
+				char c = va_arg(args, int);
+				count += write(1, &c, 1);
+			}
+			break;
+			case 's':
+			{
+				const char *str = va_arg(args, const char *);
+				while (*str)
+				{
+					count += write(1, str, 1);
+					str++;
+				}
+			}
+			break;
+			case '%':
+				count += write(1, "%", 1);
+				break;
+			default:
+				/* Unsupported conversion specifier, just print it as is */
+				count += write(1, "%", 1);
+				count += write(1, format, 1);
+				break;
+			}
+		}
+		else
+		{
+			/* Regular character, just print it */
+			count += write(1, format, 1);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
+
 
