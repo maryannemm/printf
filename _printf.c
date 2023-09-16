72d614f77dd5f83e7bddd1461013761da1315ae9
maryannemm <maryannemaree@gmail.com>
2023-09-16 10:11:11 -0700

    m

--- _printf.c
+++ _printf.c
@@ -1,58 +1,98 @@
-#include "holberton.h"
 #include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
 
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
-	int printed_chars = 0;
 	va_list args;
+	int count = 0; /* To keep track of the number of characters printed */
 
 	va_start(args, format);
 
-	if (format && args)
-	{
-		printed_chars = process_format(format, args);
-	}
-
-	va_end(args);
-	return (printed_chars);
-}
-
-int process_format(const char *format, va_list args)
-{
-	int printed_chars = 0;
-
 	while (*format)
 	{
-		if (*format != '%')
+		if (*format == '%')
 		{
-			_putchar(*format);
-			printed_chars++;
-		}
-		else if (*(format + 1))
-		{
-			format++;
-			if (*format == 'c')
-				printed_chars += print_char(args);
-			else if (*format == 's')
-				printed_chars += print_string(args);
-			else if (*format == 'd' || *format == 'i')
-				printed_chars += print_number(args);
-			else
+			format++; /* Move past '%' */
+
+			/* Handle conversion specifiers */
+			switch (*format)
 			{
-				_putchar('%');
-				_putchar(*format);
-				printed_chars += 2;
+				case 'c':
+				{
+					char c = va_arg(args, int);
+					count += write(1, &c, 1);
+				}
+				break;
+				case 's':
+				{
+					const char *str = va_arg(args, const char *);
+					while (*str)
+					{
+						count += write(1, str, 1);
+						str++;
+					}
+				}
+				break;
+				case '%':
+					count += write(1, "%", 1);
+					break;
+				case 'd':
+				case 'i':
+				{
+					int num = va_arg(args, int);
+					char buffer[12]; /* Sufficient for INT_MIN */
+					int len = 0;
+
+					if (num < 0)
+					{
+						count += write(1, "-", 1);
+						num = -num;
+					}
+
+					if (num == 0)
+					{
+						count += write(1, "0", 1);
+					}
+					else
+					{
+						while (num > 0)
+						{
+							buffer[len] = num % 10 + '0';
+							num /= 10;
+							len++;
+						}
+
+						while (len > 0)
+						{
+							len--;
+							count += write(1, &buffer[len], 1);
+						}
+					}
+				}
+				break;
+				default:
+					/* Unsupported conversion specifier, just print it as is */
+					count += write(1, "%", 1);
+					count += write(1, format, 1);
+					break;
 			}
 		}
 		else
 		{
-			_putchar('%');
-			printed_chars++;
-			break;
+			/* Regular character, just print it */
+			count += write(1, format, 1);
 		}
 		format++;
 	}
 
-	return (printed_chars);
+	va_end(args);
+	return (count);
 }
 
